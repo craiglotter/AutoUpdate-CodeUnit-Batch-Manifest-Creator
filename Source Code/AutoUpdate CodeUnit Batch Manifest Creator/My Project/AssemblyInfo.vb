182faebed765669ccf574d2da3b355244f837293
Craig Lotter <craig.lotter@gmail.com>
2014-08-01 12:38:45 +0200

    populate repo

---
+++ Source Code/AutoUpdate CodeUnit Batch Manifest Creator/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("AutoUpdate CodeUnit Batch Manifest Creator")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("CodeUnit")> 
+<Assembly: AssemblyProduct("AutoUpdate CodeUnit Batch Manifest Creator")> 
+<Assembly: AssemblyCopyright("Copyright © CodeUnit 2007")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2aaad6b0-f8c1-448a-ba7f-a6898d4e6f4d")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("2007.10.30.02")> 
+<Assembly: AssemblyFileVersion("2007.10.30.02")> 
